d89fa593f10721cdf092134fc649e2b1b0e40bc0
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-11-01 01:09:12 +0300

    Update it2.py

--- IndTasks/it2.py
+++ IndTasks/it2.py
@@ -2,10 +2,10 @@ def indtask2var2():
 
     class SFloat():
         __negative_zero: str
-        __whole: int
-        __fractional: int
+        __whole: int or str
+        __fractional: int or str
 
-        def __init__(self, whole=0, fractional=0):
+        def __init__(self, whole=0, fractional='0'):
             if isinstance(whole, str):
                 if (whole[0] == '-' and whole[1] == '0'):
                     self.__negative_zero = '-'
@@ -13,15 +13,15 @@ def indtask2var2():
                     self.__negative_zero = ''
             else: self.__negative_zero = ''
             whole = int(whole)
-            fractional = int(fractional)
+            fractional = fractional
 
             if isinstance(whole, int):
                 self.__whole = int(whole)
             else:
                 raise TypeError('Error: the \'whole\' parameter has wrong type')
-            if isinstance(fractional, int):
-                self.__fractional = int(str(divmod(float(f'0.{fractional}'), 1)[1])[2:])
-                if (fractional < 0):
+            if isinstance(fractional, int) or isinstance(fractional, str):
+                self.__fractional = fractional
+                if (float(fractional) < 0):
                     raise ValueError('Error: the \'fractional\' parameter should be greater than 0')
             else:
                 raise TypeError('Error: the \'fractional\' parameter has wrong type')
@@ -44,9 +44,9 @@ def indtask2var2():
         def GetWhole(self): return self.__whole
 
         def SetFractional(self, value):
-            if isinstance(value, int):
-                self.__fractional = int(str(divmod(float(f'0.{value}'), 1)[1])[2:])
-                if (value < 0):
+            if isinstance(value, int) or isinstance(value, str):
+                self.__fractional = value
+                if (float(value) < 0):
                     raise ValueError('Error: the \'fractional\' parameter should be greater than 0')
             else:
                 raise TypeError('Error: the \'fractional\' parameter has wrong type')
@@ -60,40 +60,40 @@ def indtask2var2():
         def __str__(self):
             return f'{self.__negative_zero}{self.__whole}.{self.__fractional}'
 
-        def __num_len__(self, num):
-            if num < 0: num = -num
-            if num == 0: return 0
-            return 1 + self.__num_len__(num // 10)
-
         def __add__(self, other):
-            integer, fract = divmod(float(str(f'0.{self.__fractional}')) + float(str(f'0.{other.__fractional}')), 1)
-            fract = round(fract, max(self.__num_len__(self.__fractional), self.__num_len__(other.__fractional)))
-            self.__whole += other.__whole + int(integer)
-            self.__fractional = int(str(fract)[2:])
+            res = float(str(self)) + float(str(other))
+            separ = str(res).split('.')
+            self.__whole = int(separ[0])
+            self.__fractional = separ[1]
             return self
 
         def __sub__(self, other):
-            temp = round(float(self) - float(other), max(self.__num_len__(self.__fractional), self.__num_len__(other.__fractional)))
-            print('temp =', temp)
-            w_f = str(temp).split('.')
-            self.__whole = int(w_f[0])
-            self.__fractional = int(w_f[1])
+            res = float(str(self)) - float(str(other))
+            separ = str(res).split('.')
+            self.__whole = int(separ[0])
+            self.__fractional = separ[1]
             return self
 
         def __mul__(self, other):
-            temp = round(float(self) * float(other), max(self.__num_len__(self.__fractional), self.__num_len__(other.__fractional)))
-            print(temp)
-            w_f = str(temp).split('.')
-            self.__whole = int(w_f[0])
-            self.__fractional = int(w_f[1])
+            res = float(str(self)) * float(str(other))
+            separ = str(res).split('.')
+            self.__whole = int(separ[0])
+            self.__fractional = separ[1]
             return self
 
-    fl = SFloat('4', 5)
-    fl2 = SFloat('2', 4)
-    print(fl)
-    print(fl2)
-    print(fl * fl2)
+    fl = SFloat(4, 42)
+    fl2 = SFloat('2', 43)
+    print('fl =', fl)
 
-# demo
-if __name__ == "__main__":
-    indtask2var2()
+    print(fl + fl2, "   ", 4.42 + 2.43)
+    print(SFloat(10, 100) + SFloat(10, 900), "   ", 10.100 + 10.900)
+    print(SFloat(10, '0123') + SFloat(10, '0127'), "   ", 10.0123 + 10.0127)
+    print(SFloat(-10, '0123') - SFloat(10, '0127'), "   ", -10.0123 - 10.0127)
+    print(SFloat('-10', '05') * SFloat(10, 12), "   ", -10.05 * 10.12)
+
+    fl.SetFractional(55)
+    print(fl)
+    fl.SetWhole('10')
+    print(fl)
+
+    print(f'{fl.GetWhole()}.{fl.GetFractional()}')
