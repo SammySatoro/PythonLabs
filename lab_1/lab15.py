1f618f76e97aa80ee60162873e67f4c74852e148
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-11-18 00:59:24 +0300

    Update lab15.py

--- lab_1/lab15.py
+++ lab_1/lab15.py
@@ -1,76 +1,108 @@
-def task15():
-    import sqlite3
-    from sqlite3 import Error
+def lab15():
+    class PyServerDB:
+        import sqlite3
+        from sqlite3 import Error
 
-    def sql_connection():
-        try:
-            db = sqlite3.connect('pyserver.db')
-            return db
-        except Error:
-            print(Error)
+        __db: sqlite3.Connection
+        __cursor: sqlite3.Cursor
 
-    def sql_print_table(table):
-        with con:
-            cursorObj.execute(f"SELECT * FROM {table}")
-            print(cursorObj.fetchall())
+        def __init__(self, db_name):
+            import sqlite3
+            from sqlite3 import Error
+            try:
+                self.__db = sqlite3.connect(db_name)
+                self.__cursor = self.__db.cursor()
+            except Error:
+                print(Error)
 
-    def sql_print_tables():
-        with con:
-            cursorObj.execute('SELECT name from sqlite_master where type= "table"')
-            print(cursorObj.fetchall())
+        def sent_query(self, query):
+            from sqlite3 import Error
+            try:
+                self.__cursor.execute(query)
+            except Error:
+                print(Error)
 
-    def sql_table(query):
-        cursorObj.execute(query)
-        con.commit()
+        def create_table(self, query):
+            self.__cursor.execute(query)
+            self.__db.commit()
 
 
-    def sql_drop_table(table):
-        try:
-            cursorObj.execute(f'DROP TABLE {table}')
-            con.commit()
-            print(f'\'{table}\' table has been deleted.')
-        except Error:
-            print('Table with such name don\'t exist.')
+        def get_table_names(self):
+            return self.__cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
-    def sql_insert(table, fields, values):
-        placeholders = ', '.join('?' for i in fields)
-        query = f"INSERT INTO {table}({', '.join(i for i in fields)}) VALUES ({placeholders})"
-        try:
-            cursorObj.execute(f"SELECT * FROM {table}")
-            if values not in cursorObj.fetchall():
-                cursorObj.execute(query, values)
-                con.commit()
+        def get_table_data(self, table):
+            return list(self.__cursor.execute(f'SELECT * FROM {table}'))
+
+        def drop_table(self, table):
+            from sqlite3 import Error
+            try:
+                self.__cursor.execute(f'DROP TABLE {table}')
+                self.__db.commit()
+                print(f'\'{table}\' table has been deleted.')
+            except Error:
+                print('Table with such name don\'t exist.')
+
+        def insert(self, table, fields, values):
+            from sqlite3 import Error
+            placeholders = ', '.join('?' for _ in fields)
+            query = f"INSERT INTO {table}({', '.join(i for i in fields)}) VALUES ({placeholders})"
+            try:
+                self.__cursor.execute(f"SELECT * FROM {table}")
+                if values not in self.__cursor.fetchall():
+                    self.__cursor.execute(query, values)
+                    self.__db.commit()
+                else:
+                    print('This entry already exists')
+                    for value in self.__cursor.execute(f"SELECT * FROM {table}"):
+                        print(value)
+            except Error:
+                print('Entry with such entry ID already exists.\nTable:', table)
+
+        def insert_many(self, table, fields, values):
+            from sqlite3 import Error
+            placeholders = ', '.join('?' for _ in fields)
+            query = f"INSERT INTO {table}({', '.join(i for i in fields)}) VALUES ({placeholders})"
+            print(query)
+            try:
+                self.__cursor.execute(f"SELECT * FROM {table}")
+                if values not in self.__cursor.fetchall():
+                    self.__cursor.executemany(query, values)
+                    self.__db.commit()
+                else:
+                    print('This entry already exists')
+                    for value in self.__cursor.execute(f"SELECT * FROM {table}"):
+                        print(value)
+            except Error:
+                print('Entry with such entry ID already exists.\nTable:', table)
+
+        def update(self, query):
+            from sqlite3 import Error
+            if query.split()[0].lower() == 'update':
+                self.__cursor.execute(query)
+                self.__db.commit()
             else:
-                print('This entry already exists')
-                for value in cursorObj.execute(f"SELECT * FROM {table}"):
-                    print(value)
-        except Error:
-            print('Entry with such entry ID already exists.\nTable:', table)
+                raise Error('Query format is not correct...')
 
-    def sql_update(query):
-        if query.split()[0].lower() == 'update':
-            print('Yep')
-            cursorObj.execute(query)
-            con.commit()
-        else:
-            raise Error('Query format is not correct...')
+        def get_rowcount(self, table):
+            self.__cursor.execute(f'SELECT * FROM {table}')
+            return len(self.__cursor.fetchall())
 
-    def sql_rowcount(table):
-        cursorObj.execute(f'SELECT * FROM {table}')
-        return len(cursorObj.fetchall())
+        def close_connection(self):
+            self.__db.close()
 
-    con = sql_connection()
-    cursorObj = con.cursor()
-    sql_table("CREATE TABLE IF NOT EXISTS test(id integer PRIMARY KEY, name text)")
-    # sql_insert('test', ('id', 'name'), (0, 'Alice'))
-    # sql_update('UPDATE test SET name = \'Jinx\' where name = \'Alice\'')
-
-    sql_drop_table('test')
-    # print(sql_print_tables())
-    # print(sql_rowcount('test'))
-    # sql_print_table('test')
+        def set_foreign(self, tabel1, table2):
+            pass
 
 
-# demo
-if __name__ == "__main__":
-    task15()
+    # FOREIGN KEY (ShowID) REFERENCES Show (ShowID) ON UPDATE CASCADE
+    # FOREIGN KEY (ArenaID) REFERENCES CircusArena (ArenaID) ON UPDATE CASCADE
+    # FOREIGN KEY (ShowID) REFERENCES Show (ShowID) ON UPDATE CASCADE
+    db = PyServerDB('pyserver.db')
+
+    db.create_table('CREATE TABLE IF NOT EXISTS Ticket('
+                    'ArenaID integer not null PRIMARY KEY AUTOINCREMENT,'
+                    'ShowID integer not null,'
+                    'Cost integer not null,'
+                    'Place integer not null)')
+    for i in db.get_table_names():
+        print(i[0])
